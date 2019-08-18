84d62211c9000824234555923e58b648642f536a
dtmkeng <kang081@hotmail.com>
2019-08-19 00:29:22 +0700

    update example

--- example/main.go
+++ example/main.go
@@ -17,14 +17,20 @@ type student struct {
 var port = 8080
 
 func main() {
-	r := bmux.NewRouter()
+	r := bmux.NewRouter() // aero & mux
 	// fmt.Println("Server start at 8000...")
 	// a := mux.NewRouter()
 	r.Get("/", func(ctx bmux.Context) error {
 		return ctx.String("hello")
 	})
+	r.Get("/", func(ctx bmux.Context) error {
+		return ctx.String("hello")
+	})
+	r.Get("/user/:name", func(ctx bmux.Context) error {
+		return ctx.String(ctx.Get("name")) // get param aero
+	})
 	r.Get("/hello", func(ctx bmux.Context) error {
-		return ctx.String(ctx.Query("name"))
+		return ctx.String(ctx.Query("name")) // query param with gin
 	})
 	// r.Run() http serer
 	fmt.Println("Server start at ... ", port)
